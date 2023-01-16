2fa74c75eda9b6f0ef8da62935e3eaa137ea539b
Daniel Rico <107763710+Umbra987@users.noreply.github.com>
2023-01-16 08:22:34 -0500

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,29 @@
+date = new Date();
+hours = date.getHours();
+Minutes = date.getMinutes();
+Seconds = date.getSeconds();
+    
+if(hours<=12){
+    giroHours = hours / 12 *360;
+}
+else{
+    giroHours = hours / 24 * 360;
+}
+giroHours = giroHours + Minutes / 60 * 30;
+giroMinutes = Minutes / 60 *360;
+giroSeconds = Seconds /60 * 360;
+    
+document.querySelector(".horas").style.transform= "rotate(" + giroHours + ".deg)";
+document.querySelector(".minutos").style.transform= "rotate(" + giroMinutes + ".deg)";
+document.querySelector(".segundos").style.transform= "rotate(" + giroSeconds + ".deg)";
+
+    
 function ChangeOclock(){
 
-    let date = new Date();
-    let hours = date.getHours();
-    let Minutes = date.getMinutes();
-    let Seconds = date.getSeconds();
+    date = new Date();
+    hours = date.getHours();
+    Minutes = date.getMinutes();
+    Seconds = date.getSeconds();
     
     if(hours<=12){
         giroHours = hours / 12 *360;
@@ -19,5 +39,4 @@ function ChangeOclock(){
     document.querySelector(".minutos").style.transform= "rotate(" + giroMinutes + ".deg)";
     document.querySelector(".segundos").style.transform= "rotate(" + giroSeconds + ".deg)";
 }
-
 setInterval(ChangeOclock,1000);
